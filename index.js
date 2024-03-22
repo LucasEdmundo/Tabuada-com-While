aa690b54fbe827698041d606335726377b173fab
Lucas <lucas.donizet@gmail.com>
2024-03-22 19:51:36 -0300

    Realizando testes de tabuada com o while e do while

---
+++ index.js
@@ -0,0 +1,33 @@
+// While(Condicao){
+// <bloco de execucao>    
+//}
+
+// let a = 10
+// let b = 15
+
+// while(a <= b){
+//   //  console.log('mais ' + a)
+//     a++
+// }
+
+// do{
+//    <bloco de execucao>
+//}while(condicao)
+
+// var i = 1
+
+// do{
+//     console.log('mais '+ i)
+//     i++
+// } while(i <= 5)
+
+let i = 0
+do {
+    console.log('5 X ' + i + ' = ' + ( 5 * i) )
+    i++
+    if(i === 11){
+        console.log("Parabens pela tabuada")
+    }
+    
+    
+} while (i <= 10);
